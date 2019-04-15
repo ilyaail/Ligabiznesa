2ad81121623ec2be69ecb99ce292d478f9d55bfe
Ilya <pw.i.like@mail.ru>
2019-04-15 19:25:38 +0300

    Opana

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/**/
+jQuery(document).ready(function($){	
+
+
+$(".c_back").fancybox({ href: '#form_call_back', wrapCSS: 'modal_call_back'});
+$(".c_back2").fancybox({ href: '#form_call_back_2', wrapCSS: 'modal_call_back'});
+$(".callback_img, .ic, .callback_txt").fancybox({ href: '#form_call_back_2', wrapCSS: 'modal_call_back'});
+});
+/**/
+
+    $( document ).ready(function() {
+
+        $(".js-video-button").modalVideo({
+            youtube:{
+                controls:0,
+                nocookie: true
+            }
+        });
+    });
