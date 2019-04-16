85d50fbd0126476126decc97a64dde7ff0aca685
Ilya <pw.i.like@mail.ru>
2019-04-16 11:36:54 +0300

    added fancybox on slider

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function($){
 $(".c_back").fancybox({ href: '#form_call_back', wrapCSS: 'modal_call_back'});
 $(".c_back2").fancybox({ href: '#form_call_back_2', wrapCSS: 'modal_call_back'});
 $(".callback_img, .ic, .callback_txt").fancybox({ href: '#form_call_back_2', wrapCSS: 'modal_call_back'});
+$("#gallery").fancybox();
 });
 /**/
 
